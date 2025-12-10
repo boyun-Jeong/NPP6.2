253d3cc7fe0b1588087534891e63b5156f945237
boyun-Jeong <jeongboryun@gmail.com>
2025-12-10 16:52:28 +0900

    a

--- BASE-UI/nxui/base/html/Highcharts-11.1.0/code/es-modules/Series/Flags/FlagsPoint.js
+++ BASE-UI/nxui/base/html/Highcharts-11.1.0/code/es-modules/Series/Flags/FlagsPoint.js
@@ -18,37 +18,37 @@ const { isNumber } = U;
  *
  * */
 class FlagsPoint extends ColumnPoint {
-    constructor() {
-        /* *
-         *
-         *  Properties
-         *
-         * */
-        super(...arguments);
-        this.options = void 0;
-        this.series = void 0;
-        this.ttBelow = false;
-    }
-    /* *
-     *
-     *  Functions
-     *
-     * */
-    /**
-     * @private
-     */
-    isValid() {
-        // #9233 - Prevent from treating flags as null points (even if
-        // they have no y values defined).
-        return isNumber(this.y) || typeof this.y === 'undefined';
-    }
-    /**
-     * @private
-     */
-    hasNewShapeType() {
-        const shape = this.options.shape || this.series.options.shape;
-        return this.graphic && shape && shape !== this.graphic.symbolKey;
-    }
+	constructor() {
+		/* *
+		 *
+		 *  Properties
+		 *
+		 * */
+		super(...arguments);
+		this.options = void 0;
+		this.series = void 0;
+		this.ttBelow = false;
+	}
+	/* *
+	 *
+	 *  Functions
+	 *
+	 * */
+	/**
+	 * @private
+	 */
+	isValid() {
+		// #9233 - Prevent from treating flags as null points (even if
+		// they have no y values defined).
+		return isNumber(this.y) || typeof this.y === 'undefined';
+	}
+	/**
+	 * @private
+	 */
+	hasNewShapeType() {
+		const shape = this.options.shape || this.series.options.shape;
+		return this.graphic && shape && shape !== this.graphic.symbolKey;
+	}
 }
 /* *
  *
